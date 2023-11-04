6ef585f0a60d3591a7a8c1dbcc1f4912855da5d8
sbukhari2 <syed.bukhari2@cix.csi.cuny.edu>
2023-11-04 17:16:16 -0400

    some stuff

---
+++ frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+import Dark from '@/components/Dark'
+ 
+export default function Page() {
+  return <div className="flex flex-col justify-center items-center h-[100%]">
+    <div className=" self-end">
+        <Dark />
+    </div>
+    <SignUp />
+
+  </div>
+}
+
